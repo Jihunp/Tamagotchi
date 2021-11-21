17e22c37239b7ef38bf02113082be4b3bdee40cb
Jihunp <edwardpark.park@gmail.com>
2021-11-20 17:02:32 -0800

    Button and naming feature was added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function sanity() {
     console.log("are we sane?");
 };
 sanity();
+// name button
+function myName() {
+    let userName = document.getElementById("petName").value;
+    alert(`Your name is now "${userName}"`);
+    //resets name of your pet incase you want to change it
+    document.getElementById("petName").value = '';
+}
