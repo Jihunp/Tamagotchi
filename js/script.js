0144d39e49dd161a3b3a485113ebf1e49128745f
Jihunp <edwardpark.park@gmail.com>
2021-11-23 09:47:06 -0800

    Play button and hidden function created

--- js/script.js
+++ js/script.js
@@ -3,21 +3,27 @@ function myName() {
     let userName = document.getElementById("pet-name").value;
     if (userName !== "") {
         alert(`Your name is now "${userName}"`);
+        document.createElement("h1").textContent = `.${userName}`
 
     } else {
         let pet = "rocky";
         alert(`Your default name is rocky`);
+        document.createElement("h1").textContent = `${pet}`
     };
 };
 
-
-
-function play() {
-    hideWelcome = document.getElementById("hidden-page");
-    hideWelcome.style.display = "none";
+// hides welcome page
+function hidePlay() {
+    // maybe change class to id in css and html later depending on what I want later
+    let hideWelcome = document.querySelector(".hidden-page");
+    if (hideWelcome.style.display == "none") {
+        hideWelcome.style.display = "block"
+    } else {
+        hideWelcome.style.display = "none"
+    }
 };
 
-// don't play button change it later
+// don't play button
 function doNotPlay() {
-    console.log("why");
+    alert("why? I spent a lot of time on it :-(... pls try it out");
 };
