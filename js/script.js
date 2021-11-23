05cd87d40324bc183874170f15965e69fa18b653
Jihunp <edwardpark.park@gmail.com>
2021-11-23 08:19:24 -0800

    Created hide button instead of multiple html pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-function sanity() {
-    console.log("are we sane?");
-};
-sanity();
 // name button
 function myName() {
-    let userName = document.getElementById("petName").value;
-    alert(`Your name is now "${userName}"`);
-    //resets name of your pet incase you want to change it
-    document.getElementById("petName").value = '';
-}
+    let userName = document.getElementById("pet-name").value;
+    if (userName !== "") {
+        alert(`Your name is now "${userName}"`);
+
+    } else {
+        let pet = "rocky";
+        alert(`Your default name is rocky`);
+    };
+};
+
+
+
+function play() {
+    hideWelcome = document.getElementById("hidden-page");
+    hideWelcome.style.display = "none";
+};
+
+// don't play button change it later
+function doNotPlay() {
+    console.log("why");
+};
