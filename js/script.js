01216808f238b5cae60693000c7027b0ba3be672
Jihunp <edwardpark.park@gmail.com>
2021-11-30 07:32:10 -0800

    Exp bar

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function myName() {
 // hides welcome page
 function hidePlay() {
     // maybe change class to id in css and html later depending on what I want later
-    let hideWelcome = document.querySelector(".hidden-page");
+    let hideWelcome = document.querySelector(".welcome-page");
     if (hideWelcome.style.display == "none") {
         hideWelcome.style.display = "block"
     } else {
@@ -28,3 +28,23 @@ function doNotPlay() {
     alert("why? I spent a lot of time on it :-(... pls try it out");
 };
 
+// exp bar
+function exp() {
+    let i = 0;
+    if (i == 0) {
+        i = 1;
+        let ele = document.getElementById("exp-bar");
+        let width = 1;
+        let id = setInterval(frame, 1000);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                ele.style.width = width +"%"
+                ele.innerHTML = width + "%"
+            }
+        }
+    }
+}
