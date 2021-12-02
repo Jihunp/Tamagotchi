b21c3891bfcaa245b219e85adfd9e92cc86514b1
Jihunp <edwardpark.park@gmail.com>
2021-12-01 17:12:42 -0800

    win lose condition and music files

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
+/* Ways to improve the game
+have character limit for name input
+create one function that combines the button clicking events for:
+eat sleep and play
 
-//object Shrock
-class Shrock {
-    constructor() {
-        this.isAlive = true;
-        this.evolveOne= "";
-        this.evolveTwo= "";
-    }
-    evolve1() {
-        $("#pet-rock").attr("src", `${this.evolveOne}`)
-    }
-    evolve2() {
-        $("#pet-rock").attr("src", `${this.evolveTwo}`)
-    }
-    
+*/
+
+
+//object ogre
+const ogre = {
+    alive:true,
+    boredom: 7,
+    hunger: 6,
+    sleepiness: 7,
 }
-let ogre = new Shrock();
-
 
 //global variables
+let time = 0;
+const increment = 10;
+
+
+// let boredBar = document.getElementById("bored");
+// let boredWidth = boredBar.style.width.replace("%", "");
+
+// let hungerBar = document.getElementById("hungry");
+// let hungerWidth = hungerBar.style.width.replace("%", "");
+
+// let sleepBar = document.getElementById("sleepy");
+// let sleepWidth = sleepBar.style.width.replace("%", "");
+
 
 // name button
 $("#name-button").on("click", function myName() {
-    let userName = document.getElementById("pet-name").value;
+    const userName = document.getElementById("pet-name").value;
     if (userName !== "") {
         alert(`Your name is now "${userName}"`);
         document.getElementById("pet").innerHTML = `${userName}`
@@ -33,6 +43,10 @@ $("#name-button").on("click", function myName() {
         document.getElementById("pet").innerHTML = pet
     };
 });
+// no play button. People who don't want to play my game get punished!
+$("#no-play").on("click", function doNotPlay() {
+    window.location ="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+});
 
 // hides start up page and initializes exp bar (start up button)
 $("#play-button").on("click", function hidePlay() {
@@ -42,26 +56,38 @@ $("#play-button").on("click", function hidePlay() {
     } else {
         hideWelcome.style.display = "none";
     }
+    gameTimer();
     exp(null);
 //evolutions
     for (let i = 0; i < 4; i++) {
         setTimeout(function timer() {
-            if (i === 1) {
-                exp($("#pet-rock").attr("src", `https://tinyurl.com/yckr6pch`))
+            if(ogre.alive ===false) {
+                return;
+            } else {
+                if(i === 1) {
+                    exp($("#pet-rock").attr("src", `https://tinyurl.com/yckr6pch`),
+                     )
+                }
+                if (i === 2) {
+                    exp($("#pet-rock").attr("src", `https://tinyurl.com/3yjvufab`), 
+                    )
+                }
+                if (i === 3) {
+                    alert("you won!")
+                }
             }
-            if (i === 2) {
-                exp($("#pet-rock").attr("src", `https://tinyurl.com/3yjvufab`))
-            }
-            if (i === 3) {
-                alert("you won!")
-            }
-        }, i * 10000)
-    }
+        } , i * 10000);
+         return;
+    };
 });
 //exp bar
 
-function exp(param) {
-    let i = 0;
+function exp(image, sound) {
+    if (ogre.alive === false) {
+        return;
+    }
+    else {
+        let i = 0;
     if (i == 0) {
         i = 1;
         let ele = document.getElementById("exp-bar");
@@ -69,7 +95,8 @@ function exp(param) {
         let id = setInterval(frame, 100);
         function frame() {
             if (width >= 100) {
-                param
+                image;
+                sound; 
                 clearInterval(id);
                 i = 0;
                 
@@ -81,27 +108,68 @@ function exp(param) {
         }
     }
 }
-
-
-// no play button. People who don't want to play my game get punished!
-$("#no-play").on("click", function doNotPlay() {
-    window.location ="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-});
-
+}
 // game time 
+function gameTimer() {
+    const goTime = function goTime() {
+        time++;
+        if(ogre.hunger === 0 || ogre.boredom === 0 || ogre.sleepiness === 0){
+            let falsehood = ogre.alive = false
+            falsehood;
+            clearInterval(globalTimer);
+            alert("you're dead kid")
+        }
+        if(time % 10 === 0 && ogre.boredom >= 1) {
+            $("#bored").css('width', '-=' + increment +"%");
+            ogre.boredom--;
+        }
+        if(time % 10 === 0 && ogre.hunger >= 1) {
+            $("#hungry").css('width', '-=' + increment +"%");
+            ogre.hunger--;
+
+        }
+        if(time % 10 === 0 && ogre.sleepiness >= 1) {
+            $("#sleepy").css('width', '-=' + increment +"%");
+            ogre.sleepiness--;
+        }
+    }
+const globalTimer = setInterval(goTime, 100)
+}
 
 
-
-// play button
+// play with pet button
 $("#play").on("click",function playPet() {
     let boredBar = document.getElementById("bored")
-        let increment = 10;
         let width = boredBar.style.width.replace("%", "");
-        console.log(width)
         if (width <= 100) {
             $("#bored").css('width', '+=' + increment +"%");
+            ogre.boredom++;
         } else {
             alert("your tamagotchi is playing too much")
+        }
+})
+
+// make pet eat 
+$("#eat").on("click",function petEat() {
+    let hungerBar = document.getElementById("hungry")
+        let width = hungerBar.style.width.replace("%", "");
+        if (width <= 100) {
+            $("#hungry").css('width', '+=' + increment +"%");
+            ogre.hunger++;
+        } else {
+            alert("your tamagotchi is eating too much")
+        }
+})
+
+// make pet sleep
+$("#sleep").on("click",function petSleep() {
+    let sleepBar = document.getElementById("sleepy")
+        let width = sleepBar.style.width.replace("%", "");
+        if (width <= 100) {
+            $("#sleepy").css('width', '+=' + increment +"%");
+            ogre.sleepiness++;
+        } else {
+            alert("your tamagotchi is sleeping too much")
         }
 })
 
